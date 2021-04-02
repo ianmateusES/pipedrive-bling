0e2f5ef6329a3e7ee34dc06cc14d955d48c450e7
ianmateusES <ec.ianmateus@alu.ufc.br>
2021-04-01 21:06:48 -0300

    feat: mongodb routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-
+import DealController from '../app/controllers/DealController';
 import DealApiPipedriveController from '../app/controllers/DealApiPipedriveController';
 
 const routes = Router();
 
+routes.get('/deals', DealController.index);
+routes.post('/deals', DealController.store);
 routes.get('/deals-api', DealApiPipedriveController.index);
 
 export default routes;
